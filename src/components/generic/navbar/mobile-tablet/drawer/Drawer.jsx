64659da611ebd80be4a6cc3969ab6146e05e9e84
3103ski <bjastski@gmail.com>
2022-06-29 17:40:31 -0700

    updates to mobile nav

--- src/components/generic/navbar/mobile-tablet/drawer/Drawer.jsx
+++ src/components/generic/navbar/mobile-tablet/drawer/Drawer.jsx
@@ -1,15 +1,15 @@
 // --> React
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // --> Packages
 import { motion } from 'framer-motion';
 
 // --> Project Imports
-// import { Button } from 'components';
+import { Button } from 'components';
 import { motion_variants_nav } from 'util';
-// import { LEAF } from 'icons';
-// import { ESTIMATE } from 'routes';
+import { SURVEY } from 'icons';
+import { REQUEST_QUOTE } from 'routes';
 
 // --> Component Imports
 import Style from './drawer.module.scss';
@@ -21,17 +21,17 @@ export default function Drawer({ open, toggle, children }) {
 				className={Style.DrawerWrapper}
 				variants={motion_variants_nav.mobile}
 				animate={open ? 'drawerOpen' : 'drawerClosed'}>
-				<div style={{ position: 'absolute', right: '20px', top: '20px' }}>
-					{/* <Button
+				<div style={{ position: 'absolute', left: '20px', top: '20px' }}>
+					<Button
 						as={Link}
-						to={ESTIMATE}
+						to={REQUEST_QUOTE}
 						className={Style.EstBtn}
 						onClick={() => toggle(false)}
-						icon={LEAF}
+						icon={SURVEY}
 						thin
 						color='secondary'>
 						Request Estimate
-					</Button> */}
+					</Button>
 				</div>
 				{children}
 			</motion.div>
